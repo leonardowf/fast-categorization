7b024d131ca1d20ac39810b0a1c6e7c0b2c34033
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2017-04-30 19:03:39 -0300

    Implementa definidor de posicionamento dos itens

--- fast-categorization/ItemExplosionMenu.swift
+++ fast-categorization/ItemExplosionMenu.swift
@@ -12,7 +12,6 @@ class ItemExplosionMenu: UIView {
     private weak var circleHighlightView: UIView?
     private weak var backgroundView: UIView?
 
-
     private var longPressing = false
 
     private var cloneOfCircleHighlightView: UIView?
@@ -73,10 +72,11 @@ class ItemExplosionMenu: UIView {
     }
 
     private func onLongPressBegan() {
-        longPressing = true
-        insertAndAnimateCircleHighlight()
-        addAndAnimateBackgroundView()
-        addCloneViews()
+//        longPressing = true
+//        insertAndAnimateCircleHighlight()
+//        addAndAnimateBackgroundView()
+//        addCloneViews()
+        addItemViews()
     }
 
     private func onLongPressEnded() {
@@ -84,6 +84,81 @@ class ItemExplosionMenu: UIView {
         animateAndRemoveCircleHightlight()
         animateAndRemoveBackgroundView()
         removeCloneViews()
+    }
+
+    private func indexOfItem(n: Int, forTotal total: Int) -> Int {
+        return 0
+    }
+
+    private func pointAtDegree(zeroPoint: CGPoint, degree: Int, radius: CGFloat) -> CGPoint {
+        let x = zeroPoint.x + radius * cos(degree.degreesToRadians)
+        let y = zeroPoint.y + radius * sin(degree.degreesToRadians)
+
+        let point = CGPoint(x: x, y: y)
+        return point
+    }
+
+    private func addItemViews() {
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+
+        let centerPoint = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        let zeroPoint = self.convert(centerPoint, to: window)
+        let r: CGFloat = 90
+
+        var points: [CGPoint] = []
+
+//        for index in 0...360 {
+//            let point = pointAtDegree(degree: index)
+//            points.append(point)
+//        }
+
+        let numberOfItems = 7
+
+        let degreesByItem = 180 / (numberOfItems - 1)
+        var evenNumberOfItensOffset = 0
+
+        if numberOfItems % 2 == 0 {
+            evenNumberOfItensOffset = degreesByItem / 2
+        } else {
+            let point = pointAtDegree(zeroPoint: zeroPoint, degree: 0, radius: r)
+            points.append(point)
+        }
+
+        for index in 1...numberOfItems / 2 {
+            let degree = index * degreesByItem - 1 * evenNumberOfItensOffset
+            let point = pointAtDegree(zeroPoint: zeroPoint, degree: degree, radius: r)
+
+            points.append(point)
+        }
+
+        for index in 1...numberOfItems / 2 {
+            let degree = -1 * index * degreesByItem + evenNumberOfItensOffset
+            let point = pointAtDegree(zeroPoint: zeroPoint, degree: degree, radius: r)
+
+            points.append(point)
+        }
+
+        points = points.sorted(by: { (lpoint, rpoint) -> Bool in
+            return lpoint.y < rpoint.y
+        })
+
+        let itemSize = CGSize(width: 20, height: 20)
+
+        points.forEach { (point) in
+            let dot = UIView(frame: CGRect(x: point.x - itemSize.width / 2, y: point.y - itemSize.height / 2, width: itemSize.width, height: itemSize.height))
+            dot.layer.cornerRadius = itemSize.width / 2
+            dot.backgroundColor = UIColor.purple
+            window.addSubview(dot)
+        }
+    }
+
+    private func rotatePointByDegree(point: CGPoint, degree: Int, c0: CGPoint) -> CGPoint {
+        let xrot = cos(degree.degreesToRadians) * (point.x - c0.x) - sin(degree.degreesToRadians) * (point.y - c0.y) + c0.x
+        let yrot = sin(degree.degreesToRadians) * (point.x - c0.x) + cos(degree.degreesToRadians) * (point.y - c0.y) + c0.y
+
+        return CGPoint(x: xrot, y: yrot)
     }
 
     private func insertAndAnimateCircleHighlight() {
@@ -195,4 +270,12 @@ class ItemExplosionMenu: UIView {
 
     }
 
+}
+
+extension Int {
+    var degreesToRadians: CGFloat { return CGFloat(self) * .pi / 180 }
+}
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
 }
